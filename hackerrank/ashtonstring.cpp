f05787d42e8db2be835e1c4e36cb786c82fd3c74
Evan Cloutier <12ec49@queensu.ca>
2016-02-10 16:15:00 -0500

    Solution for ashtonstring - need to improve efficiency with trees

--- hackerrank/ashtonstring.cpp
+++ hackerrank/ashtonstring.cpp
@@ -2,67 +2,54 @@
 #include <string>
 #include <cctype>
 #include <vector>
+#include <set>
 #include <algorithm>
 
-// Look into using hash map?
+// Look into using hash map? -- check this out
 
-void kthCharacter(const std::string& str, const int& k) {
-	// First we should create a copy of the string we are passed
+void kthCharacterSet(const std::string& str, const int& k) {
+	// First let's create a copy of the string we are passed
 	std::string inputString(str);
-	// Next we'll make a vector to store our results
-	std::vector<std::string> substrings;
-	// We'll also need a string to store for the concatenation
+	// Next we'll make a set to store our unique results
+	std::set<std::string> substringSet;
+	// And a vector to store the results afterwards
+	std::vector<std::string> substringVector;
+	// Finally we need a string to store the sorted substrings
 	std::string totalString;
 
+	// Now we can get all the substrings of the string
 	for (int i = 0; i < inputString.length(); i++) {
 		for (int j = 0; j <= inputString.length(); j++) {
-			if ((inputString.substr(i,j).empty()))
+			if (inputString.substr(i,j).empty())
 				continue;
-			substrings.push_back(inputString.substr(i,j));
+			substringSet.insert(inputString.substr(i,j));
 		}
 	}
 
-	// Now we can elimiate the duplicates in the vector
-	substrings.erase(std::unique(substrings.begin(), substrings.end()), substrings.end());
-	// Sort them lexicographically
-	std::sort(substrings.begin(), substrings.end());
+	std::copy(substringSet.begin(), substringSet.end(), std::back_inserter(substringVector));
 
-	// And transfer them into our string
-	for (int i = 0; i < substrings.size(); i++) {
-		totalString += substrings.at(i);
+	for (int i = 0; i < substringVector.size(); i++) {
+		totalString += substringVector.at(i);
 	}
 
-	std::cout << totalString[k-1] << std::endl;
+	if (k > totalString.length())
+		return;
+
+	std::cout << totalString << std::endl;
+	std::cout << totalString.at(k-1) << std::endl;
 }
 
 int main() {
-	std::string testString = "wcweojncpqwcofrhxnzgbdrd";
-	int k = 251;
-	kthCharacter(testString, k);
+    int cases, k;
+    std::string testString;
+    std::cin >> cases;
+    
+    for (int i = 0; i < cases; i++) {
+        std::cin >> testString;
+        std::cin >> k;
+        kthCharacter(testString, k);
+    }
 }
 
-/* Test Input 
 
-5
-nvzjkcahjwlhmdiuobjdwbanmvrtadopapbktdtezellktgywrdstdhhayaadqrdhspavjgxprk
-2071
-szkkcedhlkhjnjofbrnvhntsushncjavkmfn
-465
-wcweojncpqwcofrhxnzgbdrd
-251
-pfpgrnlorzzhdoxzsnemubzvkcbbfb
-77
-judaioobpoiteiszvzlscmpmpqqwuvtdqzdapudfimaowsnttalwndievaapwusmtyoksrpcfpqbkgvfiibvlkbjkcy
-2473
 
-*/
-
-/* Test Output
-
-b
-d
-d
-o
-w
-
-*/
